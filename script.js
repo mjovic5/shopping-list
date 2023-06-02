574a220a0ea6c2bd9ae0b9739c302d06b749be4d
mjovic5 <miljanjovic@ymail.com>
2023-06-02 10:03:30 +0200

    Added remove options

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearButton = document.getElementById('clear');
 
 function addItem (e) {
     e.preventDefault();
@@ -37,6 +38,20 @@ function createIcon(classes) {
     return icon;
 }
 
+function removeItem(e) {
+    if(e.target.parentElement.classList.contains('remove-item')) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 
 // Event listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItems);
